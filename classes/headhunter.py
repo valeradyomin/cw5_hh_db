8e2704d28d7ad05298e1bc20ee9d8c2f54a2edf4
Valera Demin <wahrwolffe@gmail.com>
2023-11-11 17:23:18 +0300

    add get_format_employer & clean_text

--- classes/headhunter.py
+++ classes/headhunter.py
@@ -1,5 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
+import warnings
 
+warnings.filterwarnings("ignore", category=UserWarning)
 
 
 class HeadHunter:
@@ -21,7 +24,30 @@ class HeadHunter:
         raw_data = requests.get(url=self.employer_id_url, headers=self.headers).json()
         return raw_data
 
+    def get_format_employer(self, raw_data):
+        fixed = {
+            "name": raw_data.get("name", None),
+            "description": self.clean_text(raw_data.get("description", None)),
+            "site_url": raw_data.get("site_url", None),
+        }
+        return fixed
+
+    @staticmethod
+    def clean_text(string):
+        if string:
+            soup = BeautifulSoup(string, 'html.parser')
+            clean_text = soup.get_text()
+            clean_text = clean_text.replace(
+                "\xa0", " ").replace(
+                "\n", " ").replace(
+                "\r  \r \r", " ").replace(
+                "<highlighttext>", "").replace(
+                "</highlighttext>", "")
+            return clean_text
+        else:
+            return string
+
 
 hh = HeadHunter("https://api.hh.ru/employers/23427")
 hh.check_connect()
-print(hh.get_employer())
+print(hh.get_format_employer(hh.get_employer()))
